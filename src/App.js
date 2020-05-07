e2ec15c6753e248e66a129d976a88dc1a8009f66
Kim james <dy2409@gmail.com>
2020-05-07 18:01:33 +0900

    5

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+const foodILike = [
+  {
+    id:1,
+    name: "kimchi",
+    image: "https://images.unsplash.com/photo-1583224944844-5b268c057b72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+    rating: 5
+  },
+  {
+    id:2,
+    name: "kimbap",
+    image: "https://images.unsplash.com/photo-1532347231146-80afc9e3df2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+    rating: 4.6
+  },
+  {
+    id:3,
+    name: "Doncasu",
+    image: "https://images.unsplash.com/photo-1579619002916-88cd4c81a70c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+    rating: 4
+  }
+];
+
+function Food({fav, pic, rating}){
+
+  return <div>
+           <h2>I like {fav} </h2>
+           <h4>{rating}/5.0 </h4>
+           <img  src ={pic} width="200" height="180" alt={fav} />
+  </div>
+  
+   
+}
+
+//  Food.PropTypes = {};
+
+function renderFood (dish) {
+ return <Food key = {dish.id} fav = {dish.name} pic = {dish.image } rating ={dish.rating}  />;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return  (
+    
+    <div>
+      
+      {console.log(foodILike.map(renderFood))}
+
+       
+       { foodILike.map(renderFood) }
+
+
     </div>
+    
+   
+  
   );
+  
 }
 
 export default App;
